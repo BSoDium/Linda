5a46eb5c9924292a7d3fec7b81b5095d87ffa7aa
Philippe Négrel-Jerzy <negreljerzy.philippe@gmail.com>
2021-12-01 00:21:35 +0100

    feat: :art: finish up callbacks

--- src/linda/shm/CentralizedLinda.java
+++ src/linda/shm/CentralizedLinda.java
@@ -1,5 +1,7 @@
 package src.linda.shm;
 
+import java.util.concurrent.Semaphore;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -10,31 +12,40 @@ import src.linda.Tuple;
 
 /** Shared memory implementation of Linda. */
 public class CentralizedLinda implements Linda {
-  private static ArrayList<Tuple> database;
-  // private static ArrayList<Callback> callbacks;
-  private static HashMap<Tuple, Callback> callbacks;
+  private ArrayList<Tuple> database;
+  private HashMap<Tuple, Callback> readCallbacks;
+  private HashMap<Tuple, Callback> takeCallbacks;
+  private Semaphore mutex;
 
   public CentralizedLinda() {
     database = new ArrayList<Tuple>();
+    readCallbacks = new HashMap<Tuple, Callback>();
+    takeCallbacks = new HashMap<Tuple, Callback>();
+    mutex = new Semaphore(1);
   }
 
   @Override
   public void write(Tuple t) {
+    mutex.acquireUninterruptibly();
     database.add(t);
+    runCallBacks();
+    mutex.release();
   }
 
   @Override
   public Tuple take(Tuple template) {
+    mutex.acquireUninterruptibly();
     double retryTime = 1; // seconds
 
     for (Tuple t : database) {
       if (t.matches(template)) {
         database.remove(t);
+        runCallBacks();
+        mutex.release();
         return t;
       }
     }
     // block until a matching tuple is found
-    System.out.println("No matching tuple found, waiting for a tuple to arrive...");
     while (true) {
       // wait retryTime seconds before retrying
       try {
@@ -43,11 +54,12 @@ public class CentralizedLinda implements Linda {
         e.printStackTrace();
       }
       retryTime = retryTime * 1.1;
-      System.out.println("Retrying...");
       // retry
       for (Tuple t : database) {
         if (t.matches(template)) {
           database.remove(t);
+          runCallBacks();
+          mutex.release();
           return t;
         }
       }
@@ -56,15 +68,17 @@ public class CentralizedLinda implements Linda {
 
   @Override
   public Tuple read(Tuple template) {
+    mutex.acquireUninterruptibly();
     double retryTime = 1; // seconds
 
     for (Tuple t : database) {
       if (t.matches(template)) {
+        mutex.release();
         return t;
       }
     }
+
     // block until a matching tuple is found
-    System.out.println("No matching tuple found, waiting for a tuple to arrive...");
     while (true) {
       // wait retryTime seconds before retrying
       try {
@@ -72,11 +86,11 @@ public class CentralizedLinda implements Linda {
       } catch (InterruptedException e) {
         e.printStackTrace();
       }
-      retryTime = retryTime * 1.1;
-      System.out.println("Retrying...");
+      retryTime = retryTime * 2;
       // retry
       for (Tuple t : database) {
         if (t.matches(template)) {
+          mutex.release();
           return t;
         }
       }
@@ -85,27 +99,35 @@ public class CentralizedLinda implements Linda {
 
   @Override
   public Tuple tryTake(Tuple template) {
+    mutex.acquireUninterruptibly();
     for (Tuple t : database) {
       if (t.matches(template)) {
         database.remove(t);
+        runCallBacks();
+        mutex.release();
         return t;
       }
     }
+    mutex.release();
     return null;
   }
 
   @Override
   public Tuple tryRead(Tuple template) {
+    mutex.acquireUninterruptibly();
     for (Tuple t : database) {
       if (t.matches(template)) {
+        mutex.release();
         return t;
       }
     }
+    mutex.release();
     return null;
   }
 
   @Override
   public Collection<Tuple> takeAll(Tuple template) {
+    mutex.acquireUninterruptibly();
     ArrayList<Tuple> ret = new ArrayList<Tuple>();
     for (Tuple t : database) {
       if (t.matches(template)) {
@@ -113,32 +135,110 @@ public class CentralizedLinda implements Linda {
         database.remove(t);
       }
     }
+    runCallBacks();
+    mutex.release();
     return ret;
   }
 
   @Override
   public Collection<Tuple> readAll(Tuple template) {
+    mutex.acquireUninterruptibly();
     ArrayList<Tuple> ret = new ArrayList<Tuple>();
     for (Tuple t : database) {
       if (t.matches(template)) {
         ret.add(t);
       }
     }
+    mutex.release();
     return ret;
   }
 
   @Override
   public void eventRegister(eventMode mode, eventTiming timing, Tuple template, Callback callback) {
-    callbacks.put(new Tuple(template, mode), callback);
-    // if the timing is immediate, check for matches now
-    // TODO : implement
+    // store the callback
+    if (mode == eventMode.READ) {
+      readCallbacks.put(template, callback);
+    } else if (mode == eventMode.TAKE) {
+      takeCallbacks.put(template, callback);
+    } else {
+      throw new IllegalArgumentException("Invalid event mode");
+    }
+
+    // if the event is immediate, call the callback, then remove it
+    if (timing == eventTiming.IMMEDIATE) {
+      for (Tuple t : database) {
+        if (t.matches(template)) {
+          callback.call(t);
+          if (mode == eventMode.TAKE) {
+            takeCallbacks.remove(template);
+            database.remove(t);
+          } else if (mode == eventMode.READ) {
+            readCallbacks.remove(template);
+          }
+        }
+      }
+    }
   }
 
   @Override
   public void debug(String prefix) {
-    System.out.println(prefix + ": " + database);
+    System.out.println(prefix + " Database:");
+    for (Tuple t : database) {
+      System.out.println(prefix + "  | " + t);
+    }
+    System.out.println(prefix + " Read callbacks:");
+    for (Tuple t : readCallbacks.keySet()) {
+      System.out.println(prefix + "  | " + t);
+    }
+    System.out.println(prefix + " Take callbacks:");
+    for (Tuple t : takeCallbacks.keySet()) {
+      System.out.println(prefix + "  | " + t);
+    }
   }
 
-  // TO BE COMPLETED
+  /**
+   * Run all active callbacks.
+   */
+  private void runCallBacks() {
+    for (Tuple t : database) {
+      readCallbacks.keySet().forEach(key -> {
+        if (t.matches(key)) {
+          readCallbacks.get(key).call(t);
+          readCallbacks.remove(key);
+        }
+      });
+      takeCallbacks.keySet().forEach(key -> {
+        if (t.matches(key)) {
+          takeCallbacks.get(key).call(t);
+          takeCallbacks.remove(key);
+        }
+      });
+    }
+  }
+
+  /**
+   * A simple routine to run a task and block until it is complete.
+   * <p>
+   * Note: this is currently not used.
+   * </p>
+   * 
+   * @param r
+   */
+  private void runWithRetry(Runnable r) {
+    double retryTime = 1; // seconds
+
+    while (true) {
+      // wait retryTime seconds before retrying
+      try {
+        Thread.sleep((long) (retryTime * 1000));
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      retryTime = retryTime * 2;
+      System.out.println("Retrying...");
+      // retry
+      r.run();
+    }
+  }
 
 }
